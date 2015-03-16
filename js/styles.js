d1a9ecaa0f5b606ef10cb2693c0a529b144fe95d
litzkowitz <lisa.itzkowitz@gmail.com>
2015-03-16 18:47:04 -0500

    jquery!

--- js/styles.js
+++ js/styles.js
@@ -1,5 +1,17 @@
 $(function() {
-    $("sporty").onClick(function() {
-    	$("sporty").addClass("sporty-active");
+    $(".sporty").click(function() {
+		$(this).addClass("active");
+    });
+
+    $('li').hover(function() {
+    	$(this).toggleClass("li-hover");
+    });
+
+    $('.subject li').click(function() {
+    	$(".info").removeClass('info-on');
+    	$(this).closest('.subject').addClass('subject-on');
+
+		var id = $(this).attr('data-id');
+    	$("#" + id).addClass('info-on');
     });
 });
